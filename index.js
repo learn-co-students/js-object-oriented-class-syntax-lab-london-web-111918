67054aaa7b649710cb37f4a878f24e9c1d26762a
github username <github email address>
2019-01-11 13:52:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-function BoardMember(name, homeState, training) {
+class BoardMember {
+  constructor(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
+  };
+
+  veto() {
+    return "No, I must disagree"
+  };
+
+  approve() {
+    return "You can do that!"
+  };
+
+  doCharity() {
+    return "I like to help people."
+  };
+
+  releasePressStatement() {
+    return "You will see great things from Scuber."
+  };
+
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  };
+
 };
 
-BoardMember.prototype.veto = () => "No, I must disagree"
-BoardMember.prototype.approve = () => "You can do that!"
-BoardMember.prototype.doCharity = () => "I like to help people."
-BoardMember.prototype.releasePressStatement = () => "You will see great things from Scuber."
-BoardMember.prototype.sayHi = function() {
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-};
-
-function Ceo(name, homeState, training) {
-  BoardMember.call(this, name, homeState, training);
-};
-
-Ceo.prototype.hireEmployee = () => "Welcome aboard!";
+class Ceo extends BoardMember {
+  hireEmployee() {
+    return "Welcome aboard!"
+  }
+}
